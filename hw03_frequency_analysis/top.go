02870d3c4426d9dc4310375b47e303ca9eb7b0b5
Denis Zvyagintsev <dizvyagintsev@avito.ru>
2021-07-03 20:59:00 +0300

    completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,39 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"math"
+	"regexp"
+	"sort"
+	"strings"
+)
+
+var splitWordsRegexp = regexp.MustCompile(`\s-\s|[!"#$%&'()*+,\\./:;<=>?@[\]^_\x60{|}~\s]+`)
+
+func Top10(text string) []string {
+	if text == "" {
+		return make([]string, 0)
+	}
+
+	wordFrequency := make(map[string]int)
+
+	text = strings.ToLower(text)
+	for _, word := range splitWordsRegexp.Split(text, -1) {
+		wordFrequency[word]++
+	}
+
+	words := make([]string, len(wordFrequency))
+	for word := range wordFrequency {
+		words = append(words, word)
+	}
+
+	sort.SliceStable(words, func(i, j int) bool {
+		if wordFrequency[words[i]] == wordFrequency[words[j]] {
+			return words[i] < words[j]
+		}
+
+		return wordFrequency[words[i]] > wordFrequency[words[j]]
+	})
+
+	bound := int(math.Min(10, float64(len(words))))
+	return words[:bound]
 }
