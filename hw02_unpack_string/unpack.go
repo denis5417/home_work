6251b9a6abf5343e627b7e564044c85121a480d2
Denis Zvyagintsev <dizvyagintsev@avito.ru>
2021-07-03 03:57:25 +0300

    completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,84 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+const escapeCharacter = '\\'
+
+const (
+	stringStart = iota
+	afterNumber
+	afterEscapeCharacter
+	afterRegularCharacter
+)
+
+const (
+	number = iota
+	regular
+	escape
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(packedString string) (string, error) {
+	var prevChar string
+	position := stringStart
+
+	stringBuilder := strings.Builder{}
+
+	for _, char := range packedString {
+		if position == afterEscapeCharacter {
+			if unicode.IsNumber(char) || char == escapeCharacter {
+				position = afterRegularCharacter
+				prevChar = string(char)
+				continue
+			}
+
+			return "", ErrInvalidString
+		}
+
+		switch getCharType(char) {
+		case number:
+			if position == stringStart || position == afterNumber {
+				return "", ErrInvalidString
+			}
+
+			position = afterNumber
+
+			repeatNumber, _ := strconv.Atoi(string(char))
+			stringBuilder.WriteString(strings.Repeat(prevChar, repeatNumber))
+			prevChar = ""
+		case escape:
+			position = afterEscapeCharacter
+			stringBuilder.WriteString(prevChar)
+			prevChar = ""
+		case regular:
+			stringBuilder.WriteString(prevChar)
+			prevChar = string(char)
+			position = afterRegularCharacter
+		}
+	}
+
+	if position == afterEscapeCharacter {
+		return "", ErrInvalidString
+	}
+
+	stringBuilder.WriteString(prevChar)
+
+	return stringBuilder.String(), nil
+}
+
+func getCharType(char rune) int {
+	if unicode.IsNumber(char) {
+		return number
+	}
+
+	if char == escapeCharacter {
+		return escape
+	}
+
+	return regular
 }
